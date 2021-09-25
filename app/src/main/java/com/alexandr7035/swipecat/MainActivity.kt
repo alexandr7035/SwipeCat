5704d73f4d9a7536e77a086ffa25cada2e142882
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-09-25 17:19:21 +0300

    Update nav bar and status bar color

--- app/src/main/java/com/alexandr7035/swipecat/MainActivity.kt
+++ app/src/main/java/com/alexandr7035/swipecat/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.alexandr7035.swipecat
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.alexandr7035.swipecat.cats.CatsFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.navigationBarColor = Color.TRANSPARENT
+        window.statusBarColor = Color.TRANSPARENT
+        window.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.background_primary))
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentsHost, CatsFragment.newInstance())
